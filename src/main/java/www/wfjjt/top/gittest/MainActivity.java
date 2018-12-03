26f7f84cc032c8f05fd295a3367c49fbc1141766
ypp <2435884205@qq.com>
2018-12-03 16:01:57 +0800

    First commit

---
+++ src/main/java/www/wfjjt/top/gittest/MainActivity.java
@@ -0,0 +1,13 @@
+package www.wfjjt.top.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
